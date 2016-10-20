eee5e2b296cfe1951800067fb80933c0ab68d127
David <davidchiva3@gmail.com>
2016-10-21 01:11:11 +0200

    Eliminados unos comentarios en la clase CalculatorMementoManager

--- dc.apaw.pd/src/main/java/es/upm/miw/pd/command/calculator/memento/CalculatorMementoManager.java
+++ dc.apaw.pd/src/main/java/es/upm/miw/pd/command/calculator/memento/CalculatorMementoManager.java
@@ -1,8 +1,5 @@
 package es.upm.miw.pd.command.calculator.memento;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CalculatorMementoManager extends MementoManager<String,CalculatorMemento>{
 	
 	public static MementoManager<String, CalculatorMemento> getMementoManager(){
@@ -11,16 +8,5 @@ public class CalculatorMementoManager extends MementoManager<String,CalculatorMe
 		}
 		return mementoManager;
 	}
-	
-	/*public CalculatorMemento getMemento(String key){
-		if(this.getMementos().containsKey(key)){
-			return this.getMementos().get(key);
-		}
-		return null;
-	}
-	
-	public void addMemento(String key,CalculatorMemento memento){
-		this.getMementos().put(key, memento);
-	}*/
 
 }
